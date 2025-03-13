e1213b4d7ee6c9fd324f3cd85966afc6bb7aecc5
jukiaoliveira <jurafaoliveira@icloud.com>
2025-03-13 16:28:49 -0300

    alteração no css

--- test_desempenho.py
+++ test_desempenho.py
@@ -2,6 +2,6 @@ import timeit
 from scripts import calc_imc
 
 def test_desempenho():
-    """Testa se o cálculo do IMC é realizado rapidamente (menos de 0.01s)."""
+    """Testar se o cálculo do IMC é realizado rapidamente (menos de 0.01s)."""
     tempo_execucao = timeit.timeit(lambda: calc_imc(1.75, 70), number=1000) / 1000
-    assert tempo_execucao < 0.01  # Espera que o cálculo leve menos de 10ms
+    assert tempo_execucao < 0.01  # Esperar que o cálculo leve menos de 10ms
